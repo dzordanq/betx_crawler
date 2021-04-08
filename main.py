2378a2ef46097c0b101b0d3853143d38690352a1
dzordanq <random05510@gmail.com>
2021-04-08 19:05:49 +0200

    getting only nba

--- main.py
+++ main.py
@@ -12,13 +12,16 @@ parser = Parser(config)
 
 async def main():
     events = await api.get_nba_events()
+    # events = await api.get_le_events()
     for event in events[0]['Categories'][0]['Leagues'][0]['Matches']:
         event_details = await api.get_event_details(event['Id'])
         parsed_event = parser.parse(event_details)
         print()
+        # For v1
+        v1_event = parsed_event['v1']
+        # kafka producer here
+
             
-
-
 
 loop = asyncio.get_event_loop()
 loop.run_until_complete(main())
